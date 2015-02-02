a28640c91312a1e0089fca7a058c66646c0f5b77
Neelesh <neelesh.v@cisinlabs.com>
2015-02-02 16:39:00 +0530

    intial commit

---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+Bootstrap Parallax desing + smooth scrolling
+
+Created by Script Tutorials
+License (if not mentioned otherwise in the files): http://www.script-tutorials.com/terms-of-use/
+
+Refrence code http://www.script-tutorials.com/bootstrap-one-page-template-with-parallax-effect/comment-page-1/
---
+++ js/main.js
@@ -0,0 +1,49 @@
+/**
+ *
+ * Bootstrap one-page template with Parallax effect | Script Tutorials
+ * http://www.script-tutorials.com/bootstrap-one-page-template-with-parallax-effect/
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014, Script Tutorials
+ * http://www.script-tutorials.com/
+ */
+
+$(document).ready(function (){
+
+  // create a LatLng object containing the coordinate for the center of the map
+  var latlng = new google.maps.LatLng(-33.86455, 151.209);
+
+  // prepare the map properties
+  var options = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    navigationControl: true,
+    mapTypeControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true
+  };
+
+  // initialize the map object
+  var map = new google.maps.Map(document.getElementById('google_map'), options);
+
+  // add Marker
+  var marker1 = new google.maps.Marker({
+    position: latlng, map: map
+  });
+
+  // add listener for a click on the pin
+  google.maps.event.addListener(marker1, 'click', function() {
+    infowindow.open(map, marker1);
+  });
+
+  // add information window
+  var infowindow = new google.maps.InfoWindow({
+    content:  '<div class="info"><strong>This is my company</strong><br><br>My company address is here<br> 32846 Sydney</div>'
+  });  
+
+  /*Page scroll*/
+  $.localScroll()
+});
